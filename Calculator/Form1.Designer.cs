7b8056f104b443a175f0227d5ccae690e4f6c1a0
ZaimHaqeem <93043301+ZaimHaqeem@users.noreply.github.com>
2021-10-23 22:05:10 +0800

    Add files via upload

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(358, 61);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(71, 66);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "+";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(358, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "−";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(456, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "÷";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(456, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 66);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "×";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "No. 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "No. 2";
+           // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLabel.Location = new System.Drawing.Point(63, 202);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(52, 17);
+            this.ResultLabel.TabIndex = 9;
+            this.ResultLabel.Text = "Result:";
+            this.ResultLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Calculator.Properties.Resources.Better;
+            this.pictureBox1.Location = new System.Drawing.Point(563, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 336);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddButton);
+            this.Name = "MainForm";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string No1Str;
+            string No2Str;
+
+            No1Str = textBox1.Text;
+            No2Str = textBox2.Text;
+
+            double No1Dbl;
+            double No2Dbl;
+
+            try
+            {
+                No1Dbl = Convert.ToDouble(No1Str);
+                No2Dbl = Convert.ToDouble(No2Str);
+            }
+            catch
+            {
+                ResultLabel.Text = "Please give correct value";
+                    return;
+            }
+            double ResultDbl;
+
+            ResultDbl = No1Dbl + No2Dbl;
+
+            string ResultStr;
+
+            ResultStr = Convert.ToString(ResultDbl);
+            ResultLabel.Text = "Result: "+ResultStr;
+        }
+
+       
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string No1Str;
+            string No2Str;
+
+            No1Str = textBox1.Text;
+            No2Str = textBox2.Text;
+
+            double No1Dbl;
+            double No2Dbl;
+
+            try
+            {
+                No1Dbl = Convert.ToDouble(No1Str);
+                No2Dbl = Convert.ToDouble(No2Str);
+            }
+            catch
+            {
+                ResultLabel.Text = "Please give correct value";
+                return;
+            }
+            double ResultDbl;
+
+            ResultDbl = No1Dbl * No2Dbl;
+
+            string ResultStr;
+
+            ResultStr = Convert.ToString(ResultDbl);
+            ResultLabel.Text = "Result: " + ResultStr;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string No1Str;
+            string No2Str;
+
+            No1Str = textBox1.Text;
+            No2Str = textBox2.Text;
+
+            double No1Dbl;
+            double No2Dbl;
+
+            try
+            {
+                No1Dbl = Convert.ToDouble(No1Str);
+                No2Dbl = Convert.ToDouble(No2Str);
+            }
+            catch
+            {
+                ResultLabel.Text = "Please give correct value";
+                return;
+            }
+            double ResultDbl;
+
+            ResultDbl = No1Dbl - No2Dbl;
+
+            string ResultStr;
+
+            ResultStr = Convert.ToString(ResultDbl);
+            ResultLabel.Text = "Result: " + ResultStr;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string No1Str;
+            string No2Str;
+
+            No1Str = textBox1.Text;
+            No2Str = textBox2.Text;
+
+            double No1Dbl;
+            double No2Dbl;
+
+            try
+            {
+                No1Dbl = Convert.ToDouble(No1Str);
+                No2Dbl = Convert.ToDouble(No2Str);
+            }
+            catch
+            {
+                ResultLabel.Text = "Please give correct value";
+                return;
+            }
+            double ResultDbl;
+
+            ResultDbl = No1Dbl / No2Dbl;
+
+            string ResultStr;
+
+            ResultStr = Convert.ToString(ResultDbl);
+            ResultLabel.Text = "Result: " + ResultStr;
+        }
+    }
+}
